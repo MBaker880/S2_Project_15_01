5cf1cbdc0c80bf62c4c172dd43b8e340fdb33f63
Kenzie Baker <mbaker880@west-mec.org>
2019-04-18 18:24:39 -0700

    Step 4
    On step 4, confused on how to set up the anonymous function with onchange. Moved into step 5, need to use the location.href method within the anonymous  function.

---
+++ mpl_links.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3, and JavaScript 6th Edition
+   Tutorial 13
+   Case Problem 1
+
+   Author: Mackenzie Baker
+   Date:   4.18.2019
+   
+   Filename: mpl_links.js
+
+*/
+/* ---------------------------------------------------------------------------------------------------------- */
+
+window.addEventListener("load", function () {
+    var allSelect = document.select.govLinks
+});
+
+for (var i = 0; i <= allSelect.length; i++) {
+    allSelect[i].onchange = function () {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* ---------------------------------------------------------------------------------------------------------- */
